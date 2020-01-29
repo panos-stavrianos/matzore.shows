cb2282e613a10ab6b4df46a71fbf481a28da49db
panos-stavrianos <pstavrianosmail@gmail.com>
2020-01-29 20:11:01 +0200

    fingers crossed

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,6 +8,5 @@ app = Flask(__name__, template_folder="templates")
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-login = LoginManager(app)
 
 from app import routes, models
