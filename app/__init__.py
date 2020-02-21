904958ced610a970d7f79b06e8267e6956908bcf
panos-stavrianos <pstavrianosmail@gmail.com>
2020-02-21 22:40:53 +0200

    adding "Show Playing"

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from gevent import monkey
 
 monkey.patch_all()
+from flask_socketio import SocketIO
+
 from flask import Flask
 from flask_migrate import Migrate
-from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 
 from app.config import Config
@@ -11,7 +12,6 @@ from app.config import Config
 app = Flask(__name__, template_folder="templates")
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-#db = SQLAlchemy(session_options={"autoflush": False})
 
 migrate = Migrate(app, db)
 socketio = SocketIO(app)
