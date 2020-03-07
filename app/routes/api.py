9c179780100c51b694c5784e13383c3e2c4f0064
panos-stavrianos <pstavrianosmail@gmail.com>
2020-03-07 04:49:24 +0200

    fixes -> api

--- app/routes/api.py
+++ app/routes/api.py
@@ -136,7 +136,7 @@ def api_get_categories():
 @app.route('/api/get_category/<category_id>', methods=['GET'])
 def api_get_category(category_id):
     try:
-        category = Category.query.get(category_id)
+        category = Category.query.get(category_id).to_dict()
         articles = list(map(lambda article: article.to_dict(),
                             Article.query.filter(Article.published == True, Article.category_id == category_id).all()))
         return {'articles': articles, 'category': category}
